05e674ef29439e87fbc1f0998bfffc959d2a62e1
Saul Belisle <rbelisle1270@gmail.com>
2022-03-23 20:07:14 -0500

    added customer table

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
-  state = { pageTitle: "Customers", customersCount: 5 };
+  state = {
+    pageTitle: "Customers",
+    customersCount: 5,
+    customers: [
+      {
+        id: 1,
+        name: "Scott",
+        phone: "123-4567",
+        address: { city: "Anchorage" },
+      },
+      { id: 2, name: "Tom", phone: "123-4567", address: { city: "Eugene" } },
+      { id: 3, name: "Hank", phone: "123-4567", address: { city: "Fargo" } },
+      {
+        id: 4,
+        name: "Deron",
+        phone: "123-4567",
+        address: { city: "Las Vegas" },
+      },
+      { id: 5, name: "Ruth", phone: "123-4567", address: { city: "Houston" } },
+    ],
+  };
 
   render() {
     return (
@@ -16,10 +36,32 @@ export default class App extends Component {
             Refresh
           </button>
         </h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Customer Name</th>
+              <th>Phone</th>
+              <th>City</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.customers.map((cust) => {
+              return (
+                <tr key={cust.id}>
+                  <td>{cust.id}</td>
+                  <td>{cust.name}</td>
+                  <td>{cust.phone}</td>
+                  <td>{cust.address.city}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
-  onRefreshClick() {
-    console.log("refresh clicked");
-  }
+  onRefreshClick = () => {
+    this.setState({ customersCount: 7 });
+  };
 }
