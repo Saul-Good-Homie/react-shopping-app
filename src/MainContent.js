99ef05ce190895b8ca74a55f3ce6e358543a803c
Saul Belisle <rbelisle1270@gmail.com>
2022-03-27 13:27:46 -0500

    conditional render and images

--- src/MainContent.js
+++ src/MainContent.js
@@ -10,23 +10,43 @@ export default class App extends Component {
         name: "Scott",
         phone: "123-4567",
         address: { city: "Anchorage" },
+        photo: "https://picsum.photos/id/1010/50",
       },
-      { id: 2, name: "Tom", phone: "123-4567", address: { city: "Eugene" } },
-      { id: 3, name: "Hank", phone: "123-4567", address: { city: "Fargo" } },
+      {
+        id: 2,
+        name: "Tom",
+        phone: "",
+        address: { city: "Eugene" },
+        photo: "https://picsum.photos/id/1020/50",
+      },
+      {
+        id: 3,
+        name: "Hank",
+        phone: "",
+        address: { city: "Fargo" },
+        photo: "https://picsum.photos/id/1031/50",
+      },
       {
         id: 4,
         name: "Deron",
         phone: "123-4567",
         address: { city: "Las Vegas" },
+        photo: "https://picsum.photos/id/1040/50",
       },
-      { id: 5, name: "Ruth", phone: "123-4567", address: { city: "Houston" } },
+      {
+        id: 5,
+        name: "Ruth",
+        phone: "123-4567",
+        address: { city: "Houston" },
+        photo: "https://picsum.photos/id/1050/50",
+      },
     ],
   };
 
   render() {
     return (
       <div>
-        <h3 className="border-bottom m-1 p-1">
+        <h3 className="m-1 p-1">
           {this.state.pageTitle}
 
           <span className="badge bg-secondary m-2">
@@ -45,23 +65,37 @@ export default class App extends Component {
               <th>City</th>
             </tr>
           </thead>
-          <tbody>
-            {this.state.customers.map((cust) => {
-              return (
-                <tr key={cust.id}>
-                  <td>{cust.id}</td>
-                  <td>{cust.name}</td>
-                  <td>{cust.phone}</td>
-                  <td>{cust.address.city}</td>
-                </tr>
-              );
-            })}
-          </tbody>
+          <tbody>{this.getCustomerRow()}</tbody>
         </table>
       </div>
     );
   }
   onRefreshClick = () => {
     this.setState({ customersCount: 7 });
+  };
+
+  getPhoneToRender = (phone) => {
+    if (phone) return phone;
+    else {
+      return <div className="bg-warning p-2 text-center">No Phone</div>;
+    }
+  };
+
+  getCustomerRow = () => {
+    {
+      return this.state.customers.map((cust) => {
+        return (
+          <tr key={cust.id}>
+            <td>{cust.id}</td>
+            <td>
+              <img src={cust.photo} alt="customer" />
+            </td>
+            <td>{cust.name}</td>
+            <td>{this.getPhoneToRender(cust.phone)}</td>
+            <td>{cust.address.city}</td>
+          </tr>
+        );
+      });
+    }
   };
 }
